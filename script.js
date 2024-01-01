a8cec01098aa903343cf3fbb2979a6bde268a620
1n0x3n <154578369+1n0x3n@users.noreply.github.com>
2024-01-01 11:12:06 -0600

    Add files via upload
    Project completed Successfully.

---
+++ script.js
@@ -0,0 +1,26 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+   
+    const destinations = document.querySelectorAll("#destinations .destination");
+
+    destinations.forEach(destination => {
+        destination.addEventListener("click", function () {
+            
+            alert(`You selected ${destination.querySelector("h2").textContent}`);
+        });
+    });
+
+    
+    const featuredDestinations = document.querySelectorAll("#featured .featured-destination");
+
+    featuredDestinations.forEach(destination => {
+        destination.addEventListener("click", function () {
+            
+            alert(`You selected ${destination.querySelector("h3").textContent}`);
+        });
+    });
+});
+
+
+
+
